762409d0e40543df04729741a0adea0b31fcce6e
Sadiq Shaikh <sadiq.shaikh@famekeeda.com>
2024-07-05 16:32:02 +0530

    webpage wroking fine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, url_for, session, request, redirect
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from flask_cors import CORS
-import pandas as pd
 import tempfile
 from all_functions import start
 
@@ -13,21 +12,21 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 # ------------------------------------ Adding all Routes ------------------------------------
 @app.route("/")
-def uploadPage():
-    render_template('uploadPage.html')
+def index():
+    return render_template('uploadPage.html')
 
 @app.route('/fetch', methods=['POST'])
 def fetch():
     if 'file' not in request.files:
         print('Uplaod a file to First')
-        socketio.emit('upload_status', {'status':'Uplaod a file to First'}, namespace='/uploadPage')
+        socketio.emit('upload_status', {'status':'Uplaod a file to First'}, namespace='/')
         return '', 400
     
     file = request.files['file']
 
     if file.filename == '':
         print('No File Selected')
-        socketio.emit('upload_status', {'status':'No File Selected'}, namespace='/uploadPage')
+        socketio.emit('upload_status', {'status':'No File Selected'}, namespace='/')
         return '', 400
     
     if file:
@@ -35,14 +34,16 @@ def fetch():
             file.save(temp_file.name)
             temp_file = temp_file.name
 
-        session['data'] = temp_file
+        #session['data'] = temp_file
         
         # Store upload process in the session
-        session['search_params'] = {'process': 'upload', 'data': temp_file}
-        
+        #session['search_params'] = {'process': 'upload', 'data': temp_file}
+        #output_data_path = start(temp_file)
+        print(type(temp_file))
         print('File uploaded successfully')
-        socketio.emit('upload_status', {'status': 'File uploaded successfully'}, namespace='/uploadPage')
-        
-        output_data_path = start(temp_file)
+        socketio.emit('upload_status', {'status': 'File uploaded successfully'}, namespace='/')
 
         return '', 400
+    
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
