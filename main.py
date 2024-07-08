abac7e8d5925b7e8d6b95ea29713ae97947bcd19
Sadiq Shaikh <sadiq.shaikh@famekeeda.com>
2024-07-08 12:09:23 +0530

    docker and deployment changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 # Ensure CORS is enabled
 CORS(app, resources={r"/*": {"origins": "*"}})
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
 # Temporary storage for processed file path
 temp_file_path = None
