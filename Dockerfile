87d260f668c81fb895187321751c60022f9f4edd
Sadiq Shaikh <sadiq.shaikh@famekeeda.com>
2024-07-10 15:27:37 +0530

    updated lots of things - robin

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,30 @@
+# # Use an official Python runtime as the base image
+# FROM python:3.9-slim
+
+# # Set the working directory in the container
+# WORKDIR /app
+
+# # Copy the current directory contents into the container at /app
+# COPY . /app
+
+# # Upgrade pip
+# RUN pip install --upgrade pip
+
+# # Install any needed packages specified in requirements.txt
+# RUN pip install --no-cache-dir -r requirements.txt
+
+# # Create a directory for the socket with appropriate permissions
+# RUN mkdir -p /run/gunicorn && chmod 777 /run/gunicorn
+
+# # Make port 8090 available to the world outside this container
+# EXPOSE 8090
+
+# # Define environment variable
+# ENV FLASK_APP=main.py
+
+# # Run app.py when the container launches
+# CMD ["gunicorn", "--worker-class", "eventlet", "-w", "1", "--bind", "unix:/run/gunicorn/gunicorn.sock", "--max-requests", "1000", "--timeout", "300", "--limit-request-fields", "32768", "--limit-request-field_size", "0", "main:app"]
+
 # Use an official Python runtime as the base image
 FROM python:3.9-slim
 
@@ -22,5 +49,5 @@ EXPOSE 8090
 # Define environment variable
 ENV FLASK_APP=main.py
 
-# Run app.py when the container launches
-CMD ["gunicorn", "--worker-class", "eventlet", "-w", "1", "--bind", "unix:/run/gunicorn/gunicorn.sock", "--max-requests", "1000", "--timeout", "300", "--limit-request-fields", "32768", "--limit-request-field_size", "0", "main:app"]
+# Run gunicorn when the container launches
+CMD ["gunicorn", "--worker-class", "eventlet", "-w", "1", "--bind", "unix:/run/gunicorn/gunicorn.sock", "--max-requests", "1000", "--timeout", "300", "--limit-request-line", "0", "--limit-request-fields", "32768", "--limit-request-field_size", "0", "main:app"]
--- main.py
+++ main.py
@@ -1,5 +1,133 @@
+# from flask import Flask, render_template, request, send_file
+# from flask_socketio import SocketIO
+# from all_functions import start
+# from flask_cors import CORS
+# from io import BytesIO
+# import pandas as pd
+# import traceback
+# import atexit
+# import uuid
+# import os
+
+# app = Flask(__name__)
+# app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100 MB limit
+
+# # Ensure CORS is enabled
+# CORS(app, resources={r"/*": {"origins": "*"}})
+# socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=100 * 1024 * 1024)
+
+# # Temporary storage for processed file path
+# temp_file_path = None
+
+# # Store processed data in memory
+# processed_data = {}
+
+# # ------------------------------------ Adding all Routes ------------------------------------
+# @app.route("/")
+# def index():
+#     return render_template('uploadPage.html')
+
+# @app.route('/fetch', methods=['POST', 'GET'])
+# def fetch():
+#     global temp_file_path
+#     if request.method == 'POST':
+#         if 'file' not in request.files:
+#             print('Uplaod a file to First')
+#             socketio.emit('upload_status', {'status':'Uplaod a file to First'}, namespace='/')
+#             return '', 400
+
+#         file = request.files['file']
+    
+#         if file:
+#             try:
+#                 # Process the file in memory
+#                 agency_pivotdf_dict = start(file)
+            
+#                 print('File uploaded successfully')
+#                 socketio.emit('upload_status', {'status': 'File uploaded successfully'}, namespace='/')
+
+#                 # Generate Excel file in memory
+#                 excel_file = generate_excel_file(agency_pivotdf_dict)
+
+#                 #  Generate a unique ID for this processed data
+#                 file_id = str(uuid.uuid4())
+#                 processed_data[file_id] = excel_file
+
+#                 socketio.emit('upload_status', {'status': 'File processed successfully'}, namespace='/')
+#                 return {'status': 'File processed successfully', 'file_id': file_id}, 200  # Return file_id to client
+#             except Exception as e:
+#                 error_msg = f"Error processing file: {str(e)}\n{traceback.format_exc()}"
+#                 print(error_msg)  # Print to server console
+#                 socketio.emit('upload_status', {'status': error_msg}, namespace='/')
+#                 return '', 500
+
+#     elif request.method == 'GET':
+#             file_id = request.args.get('file_id')
+#             if file_id not in processed_data:
+#                 socketio.emit('upload_status', {'status': 'No processed file available'}, namespace='/')
+#                 return '', 404
+            
+#             else:
+#                 try:
+#                     excel_file = processed_data[file_id]
+#                     excel_file.seek(0)
+#                     return send_file(
+#                         excel_file,
+#                         as_attachment=True,
+#                         download_name='output.xlsx',
+#                         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+#                     )
+#                 except Exception as e:
+#                     error_msg = f"Error downloading file: {str(e)}\n{traceback.format_exc()}"
+#                     print(error_msg)  # Print to server console
+#                     socketio.emit('upload_status', {'status': error_msg}, namespace='/')
+#                     return '', 500
+#                 finally:
+#                     socketio.sleep(1)  # Add a small delay
+#                     # Remove the processed data after sending
+#                     del processed_data[file_id]
+
+#     else:
+#         socketio.emit('upload_status', {'status': 'Unknown error occurred'}, namespace='/')
+#         return '', 405
+
+# @app.route('/cleanup', methods=['POST'])
+# def cleanup():
+#     global temp_file_path
+#     cleanup_temp_file()
+#     return '', 200
+    
+
+# # Update the cleanup function to handle potential errors
+# def cleanup_temp_file():
+#     global temp_file_path
+#     if temp_file_path and os.path.exists(temp_file_path):
+#         try:
+#             os.remove(temp_file_path)
+#             print(f"Cleaned up temporary file: {temp_file_path}")
+#         except PermissionError:
+#             print(f"Unable to remove file: {temp_file_path}. It may still be in use.")
+#         finally:
+#             temp_file_path = None
+
+# atexit.register(cleanup_temp_file)
+
+# # ------------------------------------- Other Functions ----------------------------------------
+# def generate_excel_file(agency_pivotdf_dict):
+#     output = BytesIO()
+#     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+#         for sheet_name, df in agency_pivotdf_dict.items():
+#             df.to_excel(writer, sheet_name=str(sheet_name), index=False)
+#     output.seek(0)
+#     return output
+
+# # ------------------------------------- Run the app! -------------------------------------
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 8090))
+#     socketio.run(app, host='0.0.0.0', port=port)
+
 from flask import Flask, render_template, request, send_file
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from all_functions import start
 from flask_cors import CORS
 from io import BytesIO
@@ -32,8 +160,8 @@ def fetch():
     global temp_file_path
     if request.method == 'POST':
         if 'file' not in request.files:
-            print('Uplaod a file to First')
-            socketio.emit('upload_status', {'status':'Uplaod a file to First'}, namespace='/')
+            print('Upload a file first')
+            socketio.emit('upload_status', {'status': 'Upload a file first'}, namespace='/')
             return '', 400
 
         file = request.files['file']
@@ -49,7 +177,7 @@ def fetch():
                 # Generate Excel file in memory
                 excel_file = generate_excel_file(agency_pivotdf_dict)
 
-                #  Generate a unique ID for this processed data
+                # Generate a unique ID for this processed data
                 file_id = str(uuid.uuid4())
                 processed_data[file_id] = excel_file
 
@@ -62,30 +190,30 @@ def fetch():
                 return '', 500
 
     elif request.method == 'GET':
-            file_id = request.args.get('file_id')
-            if file_id not in processed_data:
-                socketio.emit('upload_status', {'status': 'No processed file available'}, namespace='/')
-                return '', 404
-            
-            else:
-                try:
-                    excel_file = processed_data[file_id]
-                    excel_file.seek(0)
-                    return send_file(
-                        excel_file,
-                        as_attachment=True,
-                        download_name='output.xlsx',
-                        mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-                    )
-                except Exception as e:
-                    error_msg = f"Error downloading file: {str(e)}\n{traceback.format_exc()}"
-                    print(error_msg)  # Print to server console
-                    socketio.emit('upload_status', {'status': error_msg}, namespace='/')
-                    return '', 500
-                finally:
-                    socketio.sleep(1)  # Add a small delay
-                    # Remove the processed data after sending
-                    del processed_data[file_id]
+        file_id = request.args.get('file_id')
+        if file_id not in processed_data:
+            socketio.emit('upload_status', {'status': 'No processed file available'}, namespace='/')
+            return '', 404
+        
+        else:
+            try:
+                excel_file = processed_data[file_id]
+                excel_file.seek(0)
+                return send_file(
+                    excel_file,
+                    as_attachment=True,
+                    download_name='output.xlsx',
+                    mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                )
+            except Exception as e:
+                error_msg = f"Error downloading file: {str(e)}\n{traceback.format_exc()}"
+                print(error_msg)  # Print to server console
+                socketio.emit('upload_status', {'status': error_msg}, namespace='/')
+                return '', 500
+            finally:
+                socketio.sleep(1)  # Add a small delay
+                # Remove the processed data after sending
+                del processed_data[file_id]
 
     else:
         socketio.emit('upload_status', {'status': 'Unknown error occurred'}, namespace='/')
@@ -96,7 +224,6 @@ def cleanup():
     global temp_file_path
     cleanup_temp_file()
     return '', 200
-    
 
 # Update the cleanup function to handle potential errors
 def cleanup_temp_file():
